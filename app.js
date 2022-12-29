cd4ccff0907ef338b677124a972cf6785d2b0aa7
mfaizk <suneater1413@gmail.com>
2022-12-29 11:32:15 +0530

    cors added

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const auth_router = require("./router/auth.route");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 // app.use(urlencoded({}));
 app.get("/", (_req, res) => {
   res.send("I am at testing root");
