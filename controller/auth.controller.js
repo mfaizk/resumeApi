41adca59947f85d18765e7af3f3d71fe905d4fbf
mfaizk <faizk123485@gmail.com>
2023-03-25 20:13:36 +0530

    sameSite attribute changes

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -42,7 +42,7 @@ const signin = BigPromise(async (req, res) => {
   const token = await user.generateJWTToken();
   res.cookie("token", token, {
     expires: new Date(Date.now() + 2 * 24 * 60 * 60 * 1000),
-    sameSite: "none",
+    sameSite: "Lax",
   });
   user.password = undefined;
   return GlobalResponse(res, "Logged in successfully", true, 201, {
