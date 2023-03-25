2b92f5aaf4da344accdb28d90e380892061c5a32
mfaizk <faizk123485@gmail.com>
2023-03-25 13:31:31 +0530

    cookie reconfig

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -40,7 +40,7 @@ const signin = BigPromise(async (req, res) => {
   }
 
   const token = await user.generateJWTToken();
-  res.cookie("token", token, { httpOnly: true });
+  res.cookie("token", token, { httpOnly: true, maxAge: 30000, secure: false });
   user.password = undefined;
   return GlobalResponse(res, "Logged in successfully", true, 201, {
     token: token,
